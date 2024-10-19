b29faf409a85eff8052193ca00b79c8077d3c7fe
Artir Guri <artir.guri@outlook.de>
2024-10-19 20:50:28 +0200

    add rough small poke cards and some api logic

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0'
 
+//URLS
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=90&offset=0'
+let currentPokemonUrl;
+
+// DOCUMENTS
 let pokemon = [];
+let currentPokemon;
+
 
 async function init() {
     await savePokemon();
+    await renderAll();
 }
 
+
+/*LOAD FUNCTIONS */
 async function loadPokemon() {
     let response = await fetch(BASE_URL);
     return await response.json();
@@ -13,5 +22,76 @@ async function loadPokemon() {
 
 async function savePokemon() {
     pokemon = await loadPokemon();
-    console.log(pokemon)
+    console.log('pokemon ', pokemon);
+    console.log('pokemon i 6', pokemon.results[6]);
 }
+
+async function getCurrentPokemonUrl() {
+    currentPokemonUrl = await pokemon.results[0].url;
+    console.log('current url', currentPokemonUrl);
+}
+
+async function getCurrentPokemon() {
+    let response = await fetch(currentPokemonUrl);
+    console.log('res',response);
+    currentPokemon = await response.json();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*RENDER FUNCTIONS*/
+async function renderAll() {
+    await renderPokemonCardsSmall();
+}
+
+async function renderPokemonCardsSmall() {
+    await getCurrentPokemonUrl();
+    await getCurrentPokemon();
+    let pokeCardsSmallContainer = document.getElementById('poke-cards-container');
+
+    for (let i = 0; i < pokemon.results.length; i++) {
+        console.log(await getCurrentPokemonUrl(i));
+        pokeCardsSmallContainer.innerHTML += pokeCardSmallTemplate(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*TEMPLATES*/
+function pokeCardSmallTemplate(i) {
+    return (
+        `
+        <div class="poke-card-small" id="poke-card-small-${i}">
+            <img src="${currentPokemon.sprites.front_default}" alt="">
+            <div>${pokemon.results[i].name}</div>
+        </div>
+        `
+    )
+}
+
+//<img src="${currentPokemon.sprites.front_default}" alt=""/>
