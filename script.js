0a213a784816745662bdb31eee41ea0bd8e20952
Artir Guri <artir.guri@outlook.de>
2024-10-20 18:52:15 +0200

    continue work on big card

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // URLS
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=9&offset=386';
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon?limit=18&offset=386';
 let pokemonUrls = [];
 
 // variables
@@ -11,4 +11,7 @@ async function init() {
     await loadPokemonUrls();
     await loadPokemon();
     await renderAll();
+
+    console.log(pokemon[8].stats);
+    console.log(pokemon[8].stats[0].base_stat);
 }
--- scripts/template.js
+++ scripts/template.js
@@ -56,32 +56,83 @@ function pokeCardBigTemplate(i) {
                 <div>Pokedex Entry</div>
                 <div>placeholder entry</div>
             </div>
-            <div>
+            <div class="poke-info-container">
                 <div style="text-align: center">Abilites</div>
                 <div class="abilities-container">
                     <div class="poke-info-div">ability-1</div>
                     <div class="poke-info-div">ability-2</div>
                 </div>
-                <div>
+                <div class="poke-info-box">
                     <div>
-                      <div>HEIGHT</div>
+                      <div class="info-heading">HEIGHT</div>
                       <div class="poke-info-div">${pokemon[i].height + '0' + 'cm'}</div>
                     </div>
                     <div>
-                      <div>WEIGHT</div>
+                      <div class="info-heading">WEIGHT</div>
                       <div class="poke-info-div">${pokemon[i].weight + '00' + 'g'}</div>
                     </div>
-                    
+                </div>
+                <div class="poke-info-box">    
                     <div>
-                      <div>WEAKNESSES</div>
+                      <div class="info-heading">WEAKNESSES</div>
                       <div class="poke-info-div">
-                        <div>weakness placeholder</div>
+                        <div>weakness ph</div>
                       </div>
                     </div>
                     <div>
-                      <div>BASE EXP</div>
+                      <div class="info-heading">BASE EXP</div>
                       <div class="poke-info-div">${pokemon[i].base_experience}</div>
                     </div>
+                </div>
+            </div>
+            <div>
+                <div class="info-heading">STATS</div>
+                <div class="stats-container" style="text-align: center">
+                    <div>
+                        <div>HP</div>
+                        <div>${pokemon[8].stats[0].base_stat}</div>
+                    </div>
+                    <div>
+                        <div>ATK</div>
+                        <div>${pokemon[8].stats[1].base_stat}</div>
+                    </div>
+                    <div>
+                        <div>DEF</div>
+                        <div>${pokemon[8].stats[2].base_stat}</div>
+                    </div>
+                    <div>
+                        <div>SpA</div>
+                        <div>${pokemon[8].stats[3].base_stat}</div>
+                    </div>
+                    <div>
+                        <div>SpD</div>
+                        <div>${pokemon[8].stats[4].base_stat}</div>
+                    </div>
+                    <div>
+                        <div>SPD</div>
+                        <div>${pokemon[8].stats[5].base_stat}</div>
+                    </div>
+                    <div>
+                        <div>TOT</div>
+                        <div>calc total</div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>EVOLUTION</div>
+                <div class="evolution-container" >
+                    <div>img</div>
+                    <div>lvl</div>
+                    <div>img</div>
+                    <div>lvl</div>
+                    <div>img</div>
+                </div>
+            </div>
+            <div>
+                <div>prev and next</div>
+                <div>
+                    <img src="${pokemon[i-1].sprites.front_default}" alt="image of pokemon ${pokemon[i].name}">
+                    <img src="${pokemon[i+1].sprites.front_default}" alt="image of pokemon ${pokemon[i].name}">
                 </div>
             </div>
         </div>
